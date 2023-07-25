db4caea0b99312a529c449c74cfb02e0a59a1723
celestoM <130507802+celestoM@users.noreply.github.com>
2023-07-25 16:46:34 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def main():
+    return {"payload":"welcome to my project"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
